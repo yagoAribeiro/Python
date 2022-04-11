7b0f3ebdb9de94e5b334839a39bc65c60ce10638
YagoA Ribeiro <yagoamancio10@gmail.com>
2022-04-11 14:44:49 -0300

    Acrescentado todas as funcionalidades

--- projeto_conversao1/convert.py
+++ projeto_conversao1/convert.py
@@ -33,22 +33,22 @@ class Convert:
                     return print("Base não reconhecida!")
                 
                 
-            if hex == True:
-                match(char):
-                    case 'A':
-                        total += int(10)*(base**position)
-                    case 'B':
-                        total += int(11)*(base**position)
-                    case 'C':
-                        total += int(12)*(base**position)
-                    case 'D':
-                        total += int(13)*(base**position)
-                    case 'E':
-                        total += int(14)*(base**position)
-                    case 'F':
-                        total += int(15)*(base**position)
-                    case _:
-                        return print("Esse número não é de base 16!")
+            if hex == True and char not in "9876543210":
+                    match(char):
+                        case 'A':
+                            total += int(10)*(base**position)
+                        case 'B':
+                            total += int(11)*(base**position)
+                        case 'C':
+                            total += int(12)*(base**position)
+                        case 'D':
+                            total += int(13)*(base**position)
+                        case 'E':
+                            total += int(14)*(base**position)
+                        case 'F':
+                            total += int(15)*(base**position)
+                        case _:
+                            return print("Esse número não é de base 16!")
             else:
                 total += int(char)*(base**position)
             
@@ -60,5 +60,41 @@ class Convert:
         else: 
             return total
         
-    
+
+    def getFromBase(self, decimal, base):
+
+        string = ""
         
+        while(decimal>=base):
+            if base==16 and decimal%base>9:
+                string += self.getHex(decimal%base)
+            else:
+                string += str(int(decimal%base))
+            
+            decimal//= base 
+        if base==16:
+           string += self.getHex(decimal)
+        else:
+            string += str(decimal)
+        #reverter string
+        #:: = começa do último índice
+        #-1 = a leitura será feita de 1 em um índice ao contrário
+        string = string[::-1]
+        return string
+
+    
+    def getHex(self, num):
+
+            match(num):
+                case 10:
+                    return "A"
+                case 11:
+                    return "B"
+                case 12:
+                    return "C"
+                case 13:
+                    return "D"
+                case 14:
+                    return "E"
+                case 15:
+                    return "F"
--- projeto_conversao1/main.py
+++ projeto_conversao1/main.py
@@ -1,9 +1,25 @@
 import convert as Conversor
 
 conversor = Conversor.Convert()
-num_bin = input("Digite um número: \n")
-base = int(input("Digite a base desse número: \n"))
-num_decimal = conversor.getDecimal(num_bin, base)
 
-if num_decimal!= None:
-    print(f"O valor de {num_bin} na base {base} em decimal é: {num_decimal}")
+while(True):
+
+    op = input("\nDigite a operação:\n1 - Converter de outras bases para decimal\n2 - Converter de decimal para outras bases\n3 - Encerrar\n")
+    
+    match(op):
+        case "1":
+            num = input("Digite um número: \n")
+            base = int(input("Digite a base desse número: \n"))
+            num_decimal = conversor.getDecimal(num, base)
+            if num_decimal!= None:
+                print(f"\nO valor de {num} na base {base} em decimal é: {num_decimal}")
+        
+        case "2":
+            num = int(input("Digite um número decimal: \n"))
+            base = int(input("Digite a base para conversão: \n"))
+            new_num = conversor.getFromBase(num, base)
+            print(f"\nO valor de {num} decimal na base {base} é: {new_num}")
+        
+        case "3":
+            break
+print("Programa encerrado!")
